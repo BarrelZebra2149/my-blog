bb1281c35a8969613e6cb7656286421b3a737e7a
BarrelZebra2149 <1pq029@gmail.com>
2024-08-14 22:16:39 +0900

    code organization

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -6,7 +6,6 @@ const TopSection = ({flagHandle}) => {
         <div className="w3-top">
             <div className="w3-white w3-xlarge" style={{maxWidth:"1200px", margin:"auto"}}>
                 <div className="w3-button w3-padding-16 w3-left" onClick={handleOpen}>☰</div>
-                <div className="w3-right w3-padding-16">Mail</div>
                 <div className="w3-center w3-padding-16">My Food Blog</div>
             </div>
         </div>
